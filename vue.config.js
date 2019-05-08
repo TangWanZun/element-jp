e1c81b49fe76930dff46407836c5330891c0e289
TangWanZun <997017913@qq.com>
2019-05-08 18:48:44 +0800

    2019年5月8日

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   baseUrl: '/jppc/',
+
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   devServer: {
     proxy: 'https://vip.sap-unis.com/jppc/'
-  }
+  },
+
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
